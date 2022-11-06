13b520b377492e7152ae2615a84db145b5754cc3
maldeve <malandro624@gmail.com>
2022-11-07 02:37:11 +0300

    project setup

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <h4>Home</h4>
+
+    <Nav />
+</template>
+
+<script>
+import Nav from "../Shared/Nav";
+export default {
+    components: { Nav },
+};
+</script>
---
+++ resources/js/Pages/Settings.vue
@@ -0,0 +1,12 @@
+<template>
+    <h4>Settings</h4>
+
+    <Nav />
+</template>
+
+<script>
+import Nav from "../Shared/Nav";
+export default {
+    components: { Nav },
+};
+</script>
